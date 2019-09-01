6be72a62232fba97e5e2e86388d178b15c3c9743
tcooleen <tcooleen@tcooleen-pc.wireless-1x.unc.edu>
2019-09-01 17:39:26 -0400

    Got code working up to last cycle of while loop

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,59 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int totalCust = scan.nextInt();
+		// Getting the total number of customers from the first input given as an integer
+		//System.out.println("totalCust: " + totalCust);
+		
+		
+		
+		for (int i = 0; i <= totalCust; i++) {
+			// creating  for loop to reuse for as many customers as needed
+			
+			String firstName = scan.next();
+			char firstInital = firstName.charAt(0);
+			// Getting the first name of a customer from input and then using that string to get the first initial as a character
+			
+			//System.out.println("firstName: " + firstName);
+			
+			String lastName = scan.next();
+			// Getting the last name of the customer as a string from the input 
+			//System.out.println("lastName: " + lastName);
+			
+			int items = scan.nextInt();
+			// getting the items as an integer from the input
+			//System.out.println("items: " + items);
+			
+			int n = 0;
+			
+			double total = 0.00;
+			
+			while ( n < items ) {
+				
+				int quantity = scan.nextInt();
+				// quantity of item bought from input given
+				//System.out.println("quantity: " + quantity);
+				
+				String itemName = scan.next();
+				// name of item bought from input given
+				//System.out.println("itemName: " + itemName);
+				
+				double priceItem = scan.nextDouble();
+				//System.out.println("priceItem: " + priceItem);
+				
+				total = total + (quantity * priceItem);
+				// updating total with quantity and price of current item.
+				//System.out.println("total: " + total);
+				
+				n++;
+				
+			}
+			
+			System.out.println(firstInital + ". " + lastName + ": " + String.format("%.2f",total));
+		}
+		
+		scan.close();
+		// close scanner since all input has been parsed
 	}
+	
 }
